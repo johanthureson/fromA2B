502641c918ebf4cc73f1bb16e798e763b6d5879b
Johan Thureson <johan.thureson@gmail.com>
2023-08-23 12:41:42 +0200

    testTabItemTitle

--- fromA2BTests/ContentViewTests.swift
+++ fromA2BTests/ContentViewTests.swift
@@ -11,5 +11,15 @@ import ViewInspector
 @testable import fromA2B
 
 final class ContentViewTests: XCTestCase {
-
+    
+    func testTabItemTitle() throws {
+        let expected = "Trip Search"
+        let view = ContentView()
+        let tabTitle = try view
+          .inspect()
+          .find(text: expected)
+          .string()
+        XCTAssertEqual(tabTitle, expected)
+    }
+    
 }
