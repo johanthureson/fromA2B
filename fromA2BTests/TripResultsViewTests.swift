1dea51895a6457033f05b4b34b05683d149fcbe3
Johan Thureson <johan.thureson@gmail.com>
2023-08-25 10:55:31 +0200

    Added TripResultsViewtests

---
+++ fromA2BTests/TripResultsViewTests.swift
@@ -0,0 +1,12 @@
+//
+//  TripResultsViewTests.swift
+//  fromA2BTests
+//
+//  Created by Johan Thureson on 2023-08-25.
+//
+
+import XCTest
+
+final class TripResultsViewTests: XCTestCase {
+    
+}
