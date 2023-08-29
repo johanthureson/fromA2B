5a5735eaf1bd434ff05b11a781faa017b7adf698
Johan Thureson <johan.thureson@gmail.com>
2023-08-29 12:07:20 +0200

    NetworkAPITests

---
+++ fromA2BTests/NetworkAPITests.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkAPITests.swift
+//  fromA2BTests
+//
+//  Created by Johan Thureson on 2023-08-29.
+//
+
+import XCTest
+import Alamofire
+
+//final class NetworkAPITests: XCTestCase {
+//
+//    // test getStops
+//    func testGetStops() async throws {
+//        let stops = await NetworkAPI.getStops(busStopName: "Kungsgatan")
+//        XCTAssertNotNil(stops)
+//        XCTAssertEqual(stops?.count, 10)
+//        XCTAssertEqual(stops?.first?.stopLocation?.name, "Kungsgatan, Göteborg")
+//    }
+//    
+//}
