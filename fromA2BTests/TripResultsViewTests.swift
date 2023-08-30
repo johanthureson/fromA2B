160934da627facbac5996d5ded2332f4b4a213b4
Johan Thureson <johan.thureson@gmail.com>
2023-08-30 11:26:35 +0200

    Revert "Revert "testing alamofire instead""
    This reverts commit 2318f9ec0fe8c79aa2ae8885fccdc7bacda19e73.

--- fromA2BTests/TripResultsViewTests.swift
+++ fromA2BTests/TripResultsViewTests.swift
@@ -7,15 +7,16 @@
 
 import XCTest
 import Mocker
+import Alamofire
 
 final class TripResultsViewTests: XCTestCase {
     
     /// It should correctly fetch and parse the user.
     func testUserFetching() {
         
-        let configuration = URLSessionConfiguration.default
+        let configuration = URLSessionConfiguration.af.default
         configuration.protocolClasses = [MockingURLProtocol.self] + (configuration.protocolClasses ?? [])
-        let sessionManager = URLSession(configuration: configuration)
+        let sessionManager = Session(configuration: configuration)
 
         let apiEndpoint = URL(string: "https://api.resrobot.se")!// "https://api.resrobot.se/v2.1/trip?format=json&originId=740066612&destId=740046109&passlist=true&showPassingPoints=true&accessId=661da78d-bf7c-4b44-8f33-c02ebc44228a")!
         let expectedTrips = TripResponse.tripResponse!.trip!
@@ -25,19 +26,13 @@ final class TripResultsViewTests: XCTestCase {
         let mock = Mock(url: apiEndpoint, dataType: .json, statusCode: 200, data: [.get: mockedData])
         mock.register()
         
-        sessionManager.dataTask(with: apiEndpoint) { (data, response, error) in
-            defer { requestExpectation.fulfill() }
-            do {
-                if let error = error {
-                    throw error
-                }
-
-                let trips = try JSONDecoder().decode([Trip].self, from: data!)
-                XCTAssertEqual(trips, expectedTrips)
-            } catch {
-                XCTFail(error.localizedDescription)
-            }
-        }.resume()
+        sessionManager
+            .request(apiEndpoint)
+            .responseDecodable(of: [Trip].self) { (response) in
+                XCTAssertNil(response.error)
+                XCTAssertEqual(response.value, expectedTrips)
+                requestExpectation.fulfill()
+            }.resume()
 
         wait(for: [requestExpectation], timeout: 10.0)
     }
