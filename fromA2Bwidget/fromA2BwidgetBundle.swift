9ddbc970eaec960c0701681a2f6293e275a0ecc2
Johan Thureson <johan.thureson@gmail.com>
2023-08-30 13:20:04 +0200

    widget

---
+++ fromA2Bwidget/fromA2BwidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  fromA2BwidgetBundle.swift
+//  fromA2Bwidget
+//
+//  Created by Johan Thureson on 2023-08-30.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct fromA2BwidgetBundle: WidgetBundle {
+    var body: some Widget {
+        fromA2Bwidget()
+    }
+}
