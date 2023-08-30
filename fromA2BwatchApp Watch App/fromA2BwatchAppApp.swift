9cea74684255245c41757d0d3025a37c367357de
Johan Thureson <johan.thureson@gmail.com>
2023-08-30 11:58:48 +0200

    watch app target

---
+++ fromA2BwatchApp Watch App/fromA2BwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  fromA2BwatchAppApp.swift
+//  fromA2BwatchApp Watch App
+//
+//  Created by Johan Thureson on 2023-08-30.
+//
+
+import SwiftUI
+
+@main
+struct fromA2BwatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
