db23aabd2d0359e5417dc8ddd61c0eb2dc8f9009
Johan Thureson <johan.thureson@gmail.com>
2023-08-30 13:22:30 +0200

    iOS 17

--- fromA2Bwidget/fromA2Bwidget.swift
+++ fromA2Bwidget/fromA2Bwidget.swift
@@ -58,14 +58,8 @@ struct fromA2Bwidget: Widget {
 
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            if #available(iOS 17.0, *) {
-                fromA2BwidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
-            } else {
-                fromA2BwidgetEntryView(entry: entry)
-                    .padding()
-                    .background()
-            }
+            fromA2BwidgetEntryView(entry: entry)
+                .containerBackground(.fill.tertiary, for: .widget)
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
