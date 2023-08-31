61f6315244374c6ee335a260638e2589411e0bc4
Johan Thureson <johan.thureson@gmail.com>
2023-08-31 20:12:04 +0200

    StopsRequest and TripsRequest

--- fromA2B/api/request/animals/StopsRequest.swift
+++ fromA2B/api/request/animals/StopsRequest.swift
@@ -30,46 +30,61 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-enum AnimalsRequest: RequestProtocol {
-  case getAnimalsWith(page: Int, latitude: Double?, longitude: Double?)
-  case getAnimalsBy(name: String, age: String?, type: String?)
-
-  var path: String {
-    "/v2/animals"
-  }
-
-  var urlParams: [String: String?] {
-    switch self {
-    case let .getAnimalsWith(page, latitude, longitude):
-      var params = ["page": String(page)]
-      if let latitude = latitude {
-        params["latitude"] = String(latitude)
-      }
-
-      if let longitude = longitude {
-        params["longitude"] = String(longitude)
-      }
-      params["sort"] = "random"
-      return params
-
-    case let .getAnimalsBy(name, age, type):
-      var params: [String: String] = [:]
-      if !name.isEmpty {
-        params["name"] = name
-      }
-
-      if let age = age {
-        params["age"] = age
-      }
-
-      if let type = type {
-        params["type"] = type
-      }
-      return params
+enum StopsRequest: RequestProtocol {
+    
+    case getStops(busStopName: String?)
+    
+    var path: String {
+        "/location.name"
     }
-  }
+    
+    var urlParams: [String: String?] {
+        
+        switch self {
+            
+        case let .getStops(busStopName):
+            var params = ["format": "json"]
+            
+            if let busStopName {
+                params["input"] = busStopName
+            }
+            return params
+        }
+    }
+    
+    var requestType: RequestType {
+        .GET
+    }
+}
 
-  var requestType: RequestType {
-    .GET
-  }
+
+enum TripsRequest: RequestProtocol {
+    
+    case getTrips(originId: String?, destId: String?)
+    
+    var path: String {
+        "/trip"
+    }
+    
+    var urlParams: [String: String?] {
+        
+        switch self {
+            
+        case let .getTrips(originId, destId):
+            var params = ["format": "json"]
+            if let originId {
+                params["originId"] = originId
+            }
+            if let destId {
+                params["destId"] = destId
+            }
+            params["passlist"] = "true"
+            params["showPassingPoints"] = "true"
+            return params
+        }
+    }
+    
+    var requestType: RequestType {
+        .GET
+    }
 }
