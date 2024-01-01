ed0140e9b7d070805621a966389c342185b4506d
Johan Thureson <johan.thureson@gmail.com>
2024-01-01 21:50:25 +0100

    unused

--- fromA2B/api/APIConstants.swift
+++ fromA2B/api/APIConstants.swift
@@ -1,6 +1,3 @@
 enum APIConstants {
   static let host = "api.resrobot.se"
-  static let grantType = "client_credentials"
-  static let clientId = "YourKeyHere"
-  static let clientSecret = "YourSecretHere"
 }
