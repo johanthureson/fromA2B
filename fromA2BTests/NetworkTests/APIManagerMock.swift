878da903cc3f687c061447621043900ea5090030
Johan Thureson <johan.thureson@gmail.com>
2024-01-03 12:10:06 +0100

    indentation

--- fromA2BTests/NetworkTests/APIManagerMock.swift
+++ fromA2BTests/NetworkTests/APIManagerMock.swift
@@ -1,7 +1,7 @@
 import XCTest
 
 struct APIManagerMock: APIManagerProtocol {
-  func perform(_ request: RequestProtocol, authToken: String) async throws -> Data {
-    return try Data(contentsOf: URL(fileURLWithPath: request.path), options: .mappedIfSafe)
-  }
+    func perform(_ request: RequestProtocol, authToken: String) async throws -> Data {
+        return try Data(contentsOf: URL(fileURLWithPath: request.path), options: .mappedIfSafe)
+    }
 }
