6ffd7c4cb664613cb2883ecffaa1a440d93d9d59
Johan Thureson <johan.thureson@gmail.com>
2024-01-03 08:33:46 +0100

    moved loadJson

--- fromA2B/Misc/StaticFunctions.swift
+++ fromA2B/Misc/StaticFunctions.swift
@@ -9,23 +9,6 @@ import Foundation
 import KeychainSwift
 
 struct StaticFunctions {
-
-    static func loadJson<T: Decodable>(_ type: T.Type, fileName: String) -> T? {
-        
-        if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
-            do {
-                let data = try Data(contentsOf: url)
-                let decoder = JSONDecoder()
-                let jsonData = try decoder.decode(type, from: data)
-                return jsonData
-            } catch {
-                print("error:\(error)")
-            }
-        } else {
-            print()
-        }
-        return nil
-    }
     
     static func getApiKey() -> String? {
 #if DEBUG && TEST
--- fromA2B/Preview Content/StopResponse+json.swift
+++ fromA2B/Preview Content/StopResponse+json.swift
@@ -8,10 +8,29 @@
 import Foundation
 
 extension StopResponse {
-    static var originStopResponse = StaticFunctions.loadJson(StopResponse.self, fileName: "location_origin")
-    static var destinationStopResponse = StaticFunctions.loadJson(StopResponse.self, fileName: "location_destination")
+    static var originStopResponse = PreviewFunctions.loadJson(StopResponse.self, fileName: "location_origin")
+    static var destinationStopResponse = PreviewFunctions.loadJson(StopResponse.self, fileName: "location_destination")
 }
 
 extension TripResponse {
-    static var tripResponse = StaticFunctions.loadJson(TripResponse.self, fileName: "trip")
+    static var tripResponse = PreviewFunctions.loadJson(TripResponse.self, fileName: "trip")
+}
+
+struct PreviewFunctions {
+    static func loadJson<T: Decodable>(_ type: T.Type, fileName: String) -> T? {
+        
+        if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(type, from: data)
+                return jsonData
+            } catch {
+                print("error:\(error)")
+            }
+        } else {
+            print()
+        }
+        return nil
+    }
 }
