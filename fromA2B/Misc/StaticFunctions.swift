fe4a5c30aa15205860cc09aefd0cbc5f87a48490
Johan Thureson <johan.thureson@gmail.com>
2024-01-04 11:06:04 +0100

    Link to api key generation

--- fromA2B/Misc/StaticFunctions.swift
+++ fromA2B/Misc/StaticFunctions.swift
@@ -17,7 +17,8 @@ struct StaticFunctions {
      Setting up a server to host the key is not the scope of this project.
      But would be recommended otherwise.
      The worst that can happen is that someone uses my quota.
-     But easier for that person would be to get a free key of their own.
+     But easier for that person would be to get a free key of their own here:
+     https://www.trafiklab.se/api/trafiklab-apis/resrobot-v21/
      */
     static func getApiKey() -> String? {
         return "8036e1f4-06df-47e6-8477-f76939124550"
